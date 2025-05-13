0994e24e746ae3d036b41409d0936b344008bc1b
Gabriel Pavanelli <pavanelli@outlook.com>
2025-05-13 15:17:02 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Hello Pavanelli</title>
+      <title>Hello Pavanelli Success</title>
       <style>
         body {
           margin: 0;
